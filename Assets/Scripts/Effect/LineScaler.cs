902e0f6520755f117c56ea66430e566f4117e63b
Kevin Turcumani <kevinjaja98@gmail.com>
2019-10-23 09:36:47 +1100

    Initial commit

---
+++ Assets/Scripts/Effect/LineScaler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Bypasser script for allowing us to animate the scale of a LineRenderer
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineScaler : MonoBehaviour
+{
+    public float scale = 1.0f;
+    private LineRenderer lineRenderer;
+
+    private void OnDrawGizmos()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = scale;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.widthMultiplier = scale;        
+    }
+}
---
+++ Assets/Scripts/Entities/Bomb.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+using Mirror;
+
+public class Bomb : NetworkBehaviour
+{
+    public float explosionRadius = 2f;
+    public float explosionDelay = 2f;
+    public float destroyDelay = 1f;
+
+    public GameObject linePrefab;
+
+    private Animation anim;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+    void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
+    void Start()
+    {
+        StartCoroutine(Explode());
+    }
+
+    public IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explosionDelay);
+
+        Explode(transform.position, explosionRadius);
+        CmdExplode(transform.position, explosionRadius);
+
+        anim.Play("Shrink");
+
+        yield return new WaitForSeconds(destroyDelay);
+
+        NetworkServer.Destroy(gameObject);
+    }
+
+    void CreateLine(Vector3 start, Vector3 end)
+    {
+        GameObject clone = Instantiate(linePrefab, transform);
+        LineRenderer line = clone.GetComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
+    }
+
+    [Command]
+    void CmdExplode(Vector3 position, float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(position, radius);
+        foreach (var hit in hits)
+        {
+            NetworkIdentity networkId = hit.GetComponent<NetworkIdentity>();
+            if (networkId && hit.name.Contains("Enemy"))
+            {
+                NetworkServer.Destroy(hit.gameObject);
+            }
+        }
+    }
+
+    void Explode(Vector3 position, float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(position, radius);
+        foreach (var hit in hits)
+        {
+            NetworkIdentity networkId = hit.GetComponent<NetworkIdentity>();
+            if (networkId && hit.name.Contains("Enemy"))
+            {
+                CreateLine(transform.position, hit.transform.position);
+            }
+        }
+    }
+
+
+}
